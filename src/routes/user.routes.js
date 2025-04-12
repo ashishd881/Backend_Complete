0b5d0ed969e9b6ab3e0290fb92fc3a06df2690d0
ashishd881 <ashishkpdh881@gmail.com>
2025-04-12 12:49:32 +0530

    Fix typo in cover image upload route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -32,7 +32,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)       //saari cheexe update nahi kiya so we use .patch
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)         //params se le rahe hai data 
 router.route("/history").get(verifyJWT, getWatchHistory)
 
